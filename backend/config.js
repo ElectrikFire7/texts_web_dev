0cb204f59c451d87d603cb59ee5d744919f05ed4
Yolohama <9kishlay@gmail.com>
2023-12-21 18:49:27 +0530

    port_fix

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
-export const PORT = 5353;
+export const PORT = 443;
 
 export const mongoDBURL = 'mongodb+srv://9kishlay:fbZNRcgwDufZ4lvE@books-store-mern.a7tnb9k.mongodb.net/texts?retryWrites=true&w=majority'
